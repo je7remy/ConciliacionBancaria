846d2ffdba3f3c442c93357a5e23e4d8e8e098e5
Jeremy <je7remy@gmail.com>
2024-02-11 11:42:07 -0400

    procedimientos almacenados del sub-sistema contable de conciliacion bancaria parte 2

--- ConciliacionBancaria/Program.cs
+++ ConciliacionBancaria/Program.cs
@@ -16,7 +16,7 @@ namespace ConciliacionBancaria
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Conciliacion_Bancaria());
         }
     }
 }
